3086eb2393f4a932c35f5a5324fcabb8d655f65f
ars2240 <ars2240@columbia.edu>
2022-01-21 16:31:17 -0600

    fix LSTM

--- lstm_model.py
+++ lstm_model.py
@@ -17,27 +17,25 @@ class LSTM(nn.Module):
                             num_layers=num_layers, batch_first=True)
         self.fc1 = nn.Linear(hidden_size*sequence_length, self.num_classes)
         # references: https://discuss.pytorch.org/t/sequential-and-static-features-combined-in-lstm-architecture/91115
-        self.fc2 = nn.Linear(3,self.num_classes)
-        self.fc3 = nn.Linear(4,self.num_classes)
+        self.fc2 = nn.Linear(3, self.num_classes)
+        self.fc3 = nn.Linear(4, self.num_classes)
+        self.batch_size = None
+        self.hidden_cell = None
 
     def init_hidden(self, x):
         self.batch_size = x.size()[0]
-        self.hidden_cell = (torch.zeros(num_layers, self.batch_size,
-                                        hidden_size, device=device),
-                            torch.zeros(num_layers, self.batch_size,
-                                        hidden_size, device=device))
+        self.hidden_cell = (torch.zeros(num_layers, self.batch_size, hidden_size, device=device),
+                            torch.zeros(num_layers, self.batch_size, hidden_size, device=device))
 
     def forward(self, x_time, x_stat):
         # input data x
         # can use multiple inputs to forward method: https://discuss.pytorch.org/t/multiple-input-model-architecture/19754
         # for the view call: batch size, sequence length, cols
-        lstm_out, self.hidden_cell = self.lstm(x_time.view(self.batch_size,
-                                                      x_time.size()[1], -1),
-                                               self.hidden_cell)
-       # stat_out = stat_input.reshape(self.batch_size,-1)      
+        lstm_out, self.hidden_cell = self.lstm(x_time.view(self.batch_size, x_time.size()[1], -1), self.hidden_cell)
+        # stat_out = stat_input.reshape(self.batch_size,-1)
 
-        stat_preds = self.fc2(x_stat.reshape(self.batch_size,-1))
-        time_preds = self.fc1(lstm_out.reshape(self.batch_size,-1))
-        preds=torch.cat((stat_preds,time_preds),1)
+        stat_preds = self.fc2(x_stat.reshape(self.batch_size, -1))
+        time_preds = self.fc1(lstm_out.reshape(self.batch_size, -1))
+        preds=torch.cat((stat_preds, time_preds), 1)
         preds = self.fc3(preds)
         return preds.view(self.batch_size, -1)
