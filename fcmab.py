f6ed7f1379a28412842c0e95cdf7fd685bfd3afd
ars2240 <ars2240@columbia.edu>
2023-03-11 00:22:35 -0600

    tweak plots

--- fcmab.py
+++ fcmab.py
@@ -47,7 +47,7 @@ class fcmab(object):
 
         best_acc = 0
         map = 0.5
-        val_acc_list = []
+        tr_acc_list, val_acc_list, map_list, s_list = [], [], [], []
         for i in range(self.n):
 
             # open log
@@ -64,14 +64,17 @@ class fcmab(object):
                 self.model.S = self.theta > np.mean(map)
             else:
                 raise Exception('Cutoff not implemented.')
+            if not any(self.model.S):
+                self.model.S = self.theta == np.max(self.theta)
             print(self.model.S)
+            s_list.append(self.model.S)
 
             # adjust weights
             print('Iter\tEpoch\tLoss')
             self.model.train()
             for epoch in range(self.epochs):
 
-                loss_list, size, nd = [], [], []
+                loss_list, size = [], []
                 for _, data in enumerate(train_loader):
 
                     out, l, _ = self.model_loss(data)
@@ -83,52 +86,8 @@ class fcmab(object):
                     loss_list.append(l.item())
                     size.append(out.shape[0])
 
-                if self.adversarial is not None and (
-                        (type(self.adversarial) == int and self.model.S[self.adversarial]) or (
-                        type(self.adversarial) == list and all(self.model.S[a] for a in self.adversarial))):
-
-                    for _, data in enumerate(train_loader):
-
-                        _, l, _ = self.model_loss(data, adversarial=True)
-                        l.backward()
-
-                        if self.nc == 2:
-                            x0, x1, y = data
-                        elif self.nc == 4:
-                            x0, x1, x2, x3, y = data
-                        else:
-                            raise Exception("Number of clients not implemented.")
-
-                        if self.adversarial is None:
-                            raise Exception("No adversarial client selected.")
-                        if self.nc >= 1 and ((type(self.adversarial) == int and self.adversarial == 0) or
-                                             (type(self.adversarial) == list and 0 in self.adversarial)):
-                            x0.requires_grad = False
-                            x0 += self.step * x0.grad
-                        if self.nc >= 2 and ((type(self.adversarial) == int and self.adversarial == 1) or
-                                             (type(self.adversarial) == list and 1 in self.adversarial)):
-                            x1.requires_grad = False
-                            x1 += self.step * x1.grad
-                        if self.nc >= 3 and ((type(self.adversarial) == int and self.adversarial == 2) or
-                                             (type(self.adversarial) == list and 2 in self.adversarial)):
-                            x2.requires_grad = False
-                            x2 += self.step * x2.grad
-                        if self.nc >= 4 and ((type(self.adversarial) == int and self.adversarial == 3) or
-                                             (type(self.adversarial) == list and 3 in self.adversarial)):
-                            x3.requires_grad = False
-                            x3 += self.step * x3.grad
-
-                        if self.nc == 2:
-                            nd.append(utils_data.TensorDataset(x0, x1, y))
-                        elif self.nc == 4:
-                            nd.append(utils_data.TensorDataset(x0, x1, x2, x3, y))
-
-                if len(nd) > 0:
-                    nd = utils_data.ConcatDataset(nd)
-                    train_loader = utils_data.DataLoader(nd, batch_size=train_loader.batch_size,
-                                                         num_workers=train_loader.num_workers,
-                                                         pin_memory=train_loader.pin_memory)
-                    del nd
+                train_loader = self.adversary(train_loader)
+                val_loader = self.adversary(val_loader)
 
                 # scheduler.step()
                 torch.save(self.model.state_dict(), './models/' + self.head + '.pt')
@@ -136,6 +95,10 @@ class fcmab(object):
                 print("%d\t%d\t%f" % (i, epoch, loss_avg))
 
             self.model.eval()
+
+            # compute training accuracy
+            _, tr_acc = self.loss_acc(train_loader, head=self.head + '_tr')
+            tr_acc_list.append(tr_acc)
 
             # compute validation accuracy
             val_loss, val_acc = self.loss_acc(val_loader, head=self.head + '_val')
@@ -152,6 +115,7 @@ class fcmab(object):
             self.alpha += val_acc/100 * self.model.S
             self.beta += (1-val_acc/100) * self.model.S
             map = self.alpha/(self.alpha + self.beta)
+            map_list.append(map)
             print(map)
 
             # close log
@@ -178,9 +142,46 @@ class fcmab(object):
 
         if self.plot:
             check_folder('./plots')
+
+            # training plot
+            plt.plot(tr_acc_list)
+            plt.title('Training Accuracy')
+            plt.xlabel("Iterations")
+            plt.ylabel("Accuracy")
+            plt.savefig('./plots/' + self.head + '_tracc.png')
+            plt.clf()
+            plt.close()
+
+            # validation plot
             plt.plot(val_acc_list)
             plt.title('Validation Accuracy')
+            plt.xlabel("Iterations")
+            plt.ylabel("Accuracy")
             plt.savefig('./plots/' + self.head + '_valacc.png')
+            plt.clf()
+            plt.close()
+
+            # map plot
+            map_list = np.array(map_list)
+            for i in range(map_list.shape[1]):
+                plt.plot(map_list[:, i], label=str(i))
+            plt.title('MAP')
+            plt.xlabel("Iterations")
+            plt.ylabel("MAP")
+            plt.legend()
+            plt.savefig('./plots/' + self.head + '_map.png')
+            plt.clf()
+            plt.close()
+
+            # subset plot
+            s_list = -np.transpose(np.array(s_list))
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            ax.imshow(s_list, aspect='auto', cmap=plt.cm.gray, interpolation='nearest')
+            plt.title('Clients')
+            plt.xlabel("Iterations")
+            plt.ylabel("Client")
+            plt.savefig('./plots/' + self.head + '_clients.png')
             plt.clf()
             plt.close()
 
@@ -244,3 +245,49 @@ class fcmab(object):
             np.savetxt("./logs/" + head + "_conf_matrix.csv", conf, delimiter=",")
 
         return np.average(loss_list, weights=size), np.average(acc_list, weights=size)
+
+    def adversary(self, loader):
+        if self.adversarial is not None and ((type(self.adversarial) == int and self.model.S[self.adversarial]) or (
+                type(self.adversarial) == list and all(self.model.S[a] for a in self.adversarial))):
+
+            for _, data in enumerate(loader):
+
+                _, l, _ = self.model_loss(data, adversarial=True)
+                l.backward()
+
+                if self.nc == 2:
+                    x0, x1, y = data
+                elif self.nc == 4:
+                    x0, x1, x2, x3, y = data
+                else:
+                    raise Exception("Number of clients not implemented.")
+
+                if self.adversarial is None:
+                    raise Exception("No adversarial client selected.")
+                if self.nc >= 1 and ((type(self.adversarial) == int and self.adversarial == 0) or
+                                     (type(self.adversarial) == list and 0 in self.adversarial)):
+                    x0.requires_grad = False
+                    x0 += self.step * x0.grad
+                if self.nc >= 2 and ((type(self.adversarial) == int and self.adversarial == 1) or
+                                     (type(self.adversarial) == list and 1 in self.adversarial)):
+                    x1.requires_grad = False
+                    x1 += self.step * x1.grad
+                if self.nc >= 3 and ((type(self.adversarial) == int and self.adversarial == 2) or
+                                     (type(self.adversarial) == list and 2 in self.adversarial)):
+                    x2.requires_grad = False
+                    x2 += self.step * x2.grad
+                if self.nc >= 4 and ((type(self.adversarial) == int and self.adversarial == 3) or
+                                     (type(self.adversarial) == list and 3 in self.adversarial)):
+                    x3.requires_grad = False
+                    x3 += self.step * x3.grad
+
+                if self.nc == 2:
+                    nd.append(utils_data.TensorDataset(x0, x1, y))
+                elif self.nc == 4:
+                    nd.append(utils_data.TensorDataset(x0, x1, x2, x3, y))
+
+            nd = utils_data.ConcatDataset(nd)
+            loader = utils_data.DataLoader(nd, batch_size=loader.batch_size, num_workers=loader.num_workers,
+                                           pin_memory=loader.pin_memory)
+
+        return loader
