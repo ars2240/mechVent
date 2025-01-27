589a6a032f855b83c50f12e9243e7478d93524dc
ars2240 <ars2240@columbia.edu>
2025-01-27 12:43:20 -0600

    fixes

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from fcmab import main
 # m = main(data='shape', c=8, strategy=['allgood'])
 # m = main(data=['ibm'], strategy=['krum'])
 # m = main(data=['ni', 'ibm'], strategy=['mad', 'cos'])
-m = main(data=['forest', 'ni'], strategy=['krum'], use_gpu=True)
+m = main(data=['forest'], strategy=['krum'], use_gpu=True)
 m.run()
